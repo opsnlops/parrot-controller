caf84a8102fc2be13458e57b50413dcea2f87bbf
April White <april@opsnlops.io>
2022-12-22 14:28:19 -0800

    Add the Neck Rotate servo (woops!)

--- src/creature/parrot.cpp
+++ src/creature/parrot.cpp
@@ -16,6 +16,7 @@ void Parrot::init(Controller *controller) {
     // Create all of our servos
     myController->initServo(NECK_LEFT, "Neck Left", 250, 2500, false);
     myController->initServo(NECK_RIGHT, "Neck Right", 250, 2500, true);
+    myController->initServo(NECK_ROTATE, "Neck Rotate", 250, 2500, true);
     myController->initServo(BEAK, "Beak", 250, 2500, false);
     myController->initServo(CHEST, "Chest", 250, 2500, false);
     myController->initServo(BODY_LEAN, "Body Lean", 250, 2500, false);
